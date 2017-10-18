24b1428aba55c44b0a625e88138a7303a1b9e6f9
Marshall Munoz <Marshall.Munoz@fbitn.com>
2017-10-18 10:54:21 -0500

    initial files and objects created

---
+++ src/main/java/com/fbitn/digitalphoto/GameExperimentApplication.java
@@ -0,0 +1,12 @@
+package com.fbitn.digitalphoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameExperimentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fbitn/digitalphoto/GameExperimentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fbitn.digitalphoto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameExperimentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
